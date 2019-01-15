4039ad930dbbce90e5ee9e0f55c332409284d946
Andrei Gog <37507481+andreigog@users.noreply.github.com>
2019-01-15 16:37:15 +0200

    added login routing and emergency button

--- uidProject/src/app/app.component.ts
+++ uidProject/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'uidProject';
+  showNavbar = false;
+  username;
+  localStorage;
+  constructor(private router: Router) {
+    router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        this.localStorage = localStorage;
+        console.log(val.url);
+        this.showNavbar = (val.url.slice(1) !== 'loginPatient') && (val.url.slice(1) !== 'loginDoctor');
+        this.username = localStorage.getItem('username')
+      }
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('username');
+    this.router.navigate(['/loginPatient']);
+  }
 }
